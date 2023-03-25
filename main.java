e0b46b9a67c4a60c897ee1d4c55784fbe513448b
Rustem Shakirov <rustem_shakirov@yahoo.com>
2023-03-25 19:30:28 +0900

    Prettier

--- main.java
+++ main.java
@@ -6,26 +6,26 @@ class Main {
 		int card[] = new int[4];
 		char c;
 	
-		Scanner cin= new Scanner(System.in);
+		Scanner cin = new Scanner(System.in);
 		for(;cin.hasNext();){
-			int x=0;
-			String n=cin.nextLine();
-			for (int i=0; i<n.length(); i++) {
-				c=n.charAt(i);
-				card[i]=Character.digit(c,10);
-				if (card[i]==0) x=1;
+			int x = 0;
+			String n = cin.nextLine();
+			for (int i = 0; i < n.length(); i++) {
+				c = n.charAt(i);
+				card[i] = Character.digit(c,10);
+				if (card[i] == 0) x = 1;
 			}
 			
-			if (x==0) {
-				int max=card[0];
-				for (int i=0; i<n.length(); i++) {
-					if (card[i]>max) max=card[i];
+			if (x == 0) {
+				int max = card[0];
+				for (int i = 0; i < n.length(); i++) {
+					if (card[i] > max) max = card[i];
 				}
 				System.out.println(max);
 			} else {
-				int min=9;
-				for (int i=0; i<n.length(); i++) {
-					if (card[i]<min && card[i]!=0) min=card[i];
+				int min = 9;
+				for (int i = 0; i < n.length(); i++) {
+					if (card[i] < min && card[i] != 0) min = card[i];
 				}
 				System.out.println(min);
 			}
